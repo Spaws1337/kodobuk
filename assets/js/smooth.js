a0ee08ec111b033d0f6c91b97933ff3d4dd974c4
Spaws1337 <kerrymonich@yandex.ru>
2025-04-29 20:51:16 +0300

    upd

--- assets/js/smooth.js
+++ assets/js/smooth.js
@@ -9,3 +9,32 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       }
     });
   });
+
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const text = document.querySelector('.about-text');
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          text.classList.add('visible');
+        }
+      });
+    }, { threshold: 0.3 });
+
+    observer.observe(text);
+  });
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    const infoTexts = document.querySelectorAll('.info-text');
+    
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+        }
+      });
+    }, { threshold: 0.3 });
+    
+    infoTexts.forEach(el => observer.observe(el));
+  });
+  
